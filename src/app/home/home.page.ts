c13a858e7b7e83d2c55c5ad745735db65038fcb2
POAPCA02LC0310 <SSDD@POAPCA02LC0310.duoc.acad>
2024-11-08 20:10:23 -0300

    Editado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+// Exporto OnInit
+export class HomePage implements OnInit{
 
   constructor() {}
+// 2 Creo el componente con ionic g en consola
+// 3 Hago nacer el componente
+  ngOnInit(): void{
+
+  // 4 Le creo una funcion al componente
+    this.suma(1,1);
+  }
+  // 5 Creo la variable
+  suma (a:number, b:number) :number{
+    console.log(a+b);
+    return a+b;
+  }
 
 }
