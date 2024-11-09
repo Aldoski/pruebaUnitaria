9fc21267e9f65ea6933d947b62bc150deacf24b4
POAPCA02LC0102 <SSDD@POAPCA02LC0102.duoc.acad>
2024-11-09 13:37:44 -0300

    Descripción de los cambios que has realizado

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
 
+// a "component" le puedo cambiar el nombre para que ser mas facil de leer
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
@@ -21,4 +22,35 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // 6 Creo el "spy" spy en sí guarda el resultado de cada operacion
+  it ('Debería ser numero positivo', () =>{
+    const spy = spyOn (component, 'suma');
+    component.ngOnInit()
+    expect (spy).toHaveBeenCalled();
+  });
+
+  it ('Debería ser numero positivo', () =>{
+    const spy = spyOn (component, 'mult');
+    component.ngOnInit()
+    expect (spy).toHaveBeenCalled();
+  });
+
+  it ('Debería ser numero positivo', () =>{
+    const spy = spyOn (component, 'div');
+    component.ngOnInit()
+    expect (spy).toHaveBeenCalled();
+  });
+
+  it ('Debería ser numero positivo', () =>{
+    const spy = spyOn (component, 'resta');
+    component.ngOnInit()
+    expect (spy).toHaveBeenCalled();
+  });
+
+  // 10 manejo error "titulo"
+  it ('deberia decir titulo',() =>{
+    expect(component.titulo).toEqual('Home Page.');
+  });
+
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,18 +8,60 @@ import { Component, OnInit } from '@angular/core';
 // Exporto OnInit
 export class HomePage implements OnInit{
 
+  // 9 si quiero tomar otra variable
+  titulo:string = 'Home Page.'
+
+
+
   constructor() {}
 // 2 Creo el componente con ionic g en consola
 // 3 Hago nacer el componente
   ngOnInit(): void{
 
   // 4 Le creo una funcion al componente
-    this.suma(1,1);
+    this.suma(-31,1);
+    this.mult(1,1);
+    this.div (5,2);
+    this.resta(1,4);
   }
-  // 5 Creo la variable
-  suma (a:number, b:number) :number{
+
+
+  // 5 Creo la funcion operacional
+  suma (a: number, b: number): number{
+    if (a < 0 || b < 0){
+    throw new Error('Mal')
+  }
     console.log(a+b);
     return a+b;
   }
+  
+    mult (a: number, b: number): number{
+    if (a < 0 || b < 0){
+    throw new Error('Mal')
+  }
+    console.log(a*b);
+    return a*b;
+  }
+
+  div (a: number, b: number): number{
+    if (a < 0 || b < 0){
+    throw new Error('Mal')
+  }
+    console.log(a/b);
+    return a*b;
+  }
+
+  resta (a: number, b: number): number{
+    if (a < 0 || b < 0){
+    throw new Error('Mal')
+  }
+    console.log(a-b);
+    return a*b;
+  }
+
+
+
+// 7 hacer importacion de jasmine para el testeo "npm run test"
+// 8 agregar "--code-coverage" al script de "test" en el package.json
 
 }
